2198d4c40088f9175ef3d0a735d771c9e9896b2c
Serhii Krainik <42071002+Sergiyss@users.noreply.github.com>
2024-04-24 12:52:36 +0300

    Добавил форму ввода

--- front/index.php
+++ front/index.php
@@ -1,31 +1,54 @@
 <?php 
 
-
-function custom_page_html_form() { ?>
-	<div class="container">
+function settings_page_html_form() { ?>
+    <div class="container">
         <div class="row">
-
             <div class="col-12 col-sm-12">
-                <h2>Налаштування плагіна Contact Form 7 Lead Generation</h2>
-            </div>
-
-
-            <div class="col-12 col-sm-12">
-                <form method="post" action="options.php">
-                    <?php settings_fields('custom_plugin_options_group'); ?>
-         
-                    <table class="form-table">
-                        <tr>
-                            <th><label for="first_field_id">Введіть ключ:</label></th>
-                            <td>
-                                <input type = 'text' class="regular-text" id="first_field_id" name="key_ifteam" value="<?php echo get_option('key_ifteam'); ?>">
-                            </td>
-                        </tr>
-                    </table>
-                </form>
-            <?php submit_button(); ?>
+                <h1>Налаштування плагіна Contact Form 7 Lead Generation</h1>
             </div>
         </div>
+      
+        <form method="post" action="options.php">
+            <?php
+                settings_fields('my_plugin_settings');
+                do_settings_sections('my_plugin_settings');
+                submit_button('Save Settings');
+            ?>
+        </form>
     </div>
 <?php } ?>
 
+<?php   
+
+// Регистрируем секции и поля настроек
+add_action('admin_init', 'my_plugin_initialize_settings');
+
+function my_plugin_initialize_settings() {
+
+        add_settings_section(
+            'my_plugin_general_settings_section',
+            'Настройка отправителя',
+            'my_plugin_general_settings_section_callback',
+            'my_plugin_settings'
+        );
+
+        add_settings_field(
+            'np_settings_apiKey',
+            'ApiKey',
+            'np_settings_apiKey_form',
+            'my_plugin_settings',
+            'my_plugin_general_settings_section'
+        );
+        
+        register_setting(
+            'my_plugin_settings',
+            'np_settings_apiKey',
+        );
+
+        function np_settings_apiKey_form() {
+            $form = get_option('np_settings_apiKey');
+            ?>
+            <input type="string" name="ifteam_apiKey" value="<?php echo esc_attr($form); ?>" />
+        <?php } ?>
+
+<?php } ?>
