80aa671848996956a6d3f23e4ec4fed0ce893590
pc02 <weert19933@gmail.com>
2024-04-24 13:35:03 +0300

    Обновление базовой базы

--- cf7-lead-generation.php
+++ cf7-lead-generation.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Contact Form 7 Lead Generation by if.team
- * Description: A powerful & beautiful documentation, knowledge base builder plugin.
+ * Description: Плагін з інтреграцією з Contact Form 7 для лідогенерації з сервісом if.team.
  * Plugin URI: https://if.team
  * Author: if.team
  * Author URI: https://if.team
@@ -14,15 +14,15 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit();
+    exit();
 }
 
 
-include_one '/front/index.php'; 
+include 'front/index.php'; 
 
 
 function cf7_lg_scripts() {
-	wp_enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js', array('jquery'), '5.3.3', true);
+    wp_enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js', array('jquery'), '5.3.3', true);
     wp_enqueue_script('main', plugin_dir_url( __FILE__ ) . 'assets/js/main.js', array('jquery'), '1.0', true);
     wp_localize_script( 'main', 'myAjax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ))); 
 }
@@ -33,7 +33,7 @@ add_action('wp_enqueue_scripts', 'cf7_lg_scripts');
 
 function cf7_lg_style() {
 
-	wp_enqueue_style( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css', array(), '5.3.3', 'all' );
+    wp_enqueue_style( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css', array(), '5.3.3', 'all' );
     wp_enqueue_style( 'my-style', plugin_dir_url( __FILE__ ) . 'assets/css/style.css', array(), '1.0', 'all' );
 }
 add_action( 'wp_enqueue_scripts', 'cf7_lg_style' );
@@ -45,21 +45,21 @@ function  activate_pulugin(){}
 register_activation_hook(__FILE__, 'activate_pulugin');
 
 
-function custom_cf7_plugin_menu() {
+function cf7lg_plugin_menu() {
     add_submenu_page(
         'wpcf7',
         'Custom CF7 Plugin',
         'Custom CF7 Plugin',
         'manage_options',
         'custom-cf7-plugin',
-        'custom_page_html_form'
+        'settings_page_html_form'
     );
 }
 
-function custom_cf7_plugin_page_callback() {
-    // Содержимое вашей страницы плагина
-    echo '<div class="wrap"><h2>Custom CF7 Plugin Page</h2><p>Это содержимое вашей страницы плагина.</p></div>';
-}
+add_action( 'admin_menu', 'cf7lg_plugin_menu' );
+
+
+
 
 add_action ( 'wpcf7_mail_sent' , 'store_cf7_data_in_local_storage' ); 
 
@@ -76,14 +76,10 @@ function  store_cf7_data_in_local_storage ( $contact_form )   {
         $json_data = json_encode ( $posted_data ); 
 
         // Сохраняем данные в локальном хранилище 
-        echo  "<script>Conosole.log(" . $json_data . ");</script>" ; 
-		
-		mail("serhii.kr93@gmail.com","Test","".$json_data);
-		
+        $json_data = json_encode( $posted_data, JSON_UNESCAPED_UNICODE );
+        mail( "serhii.kr93@gmail.com", "Test", $json_data );
+        
     } 
 }
-
-
-
 
 ?>
--- front/index.php
+++ front/index.php
@@ -1,31 +1,65 @@
 <?php 
 
 
-function custom_page_html_form() { ?>
-	<div class="container">
-        <div class="row">
-
+function settings_page_html_form() { ?>
+    <div class="container">
+        <!-- <div class="row">
             <div class="col-12 col-sm-12">
-                <h2>Налаштування плагіна Contact Form 7 Lead Generation</h2>
+                <h1>Налаштування плагіна Contact Form 7 Lead Generation</h1>
             </div>
-
-
-            <div class="col-12 col-sm-12">
-                <form method="post" action="options.php">
-                    <?php settings_fields('custom_plugin_options_group'); ?>
-         
-                    <table class="form-table">
-                        <tr>
-                            <th><label for="first_field_id">Введіть ключ:</label></th>
-                            <td>
-                                <input type = 'text' class="regular-text" id="first_field_id" name="key_ifteam" value="<?php echo get_option('key_ifteam'); ?>">
-                            </td>
-                        </tr>
-                    </table>
-                </form>
-            <?php submit_button(); ?>
-            </div>
-        </div>
+        </div> -->
+      
+        <form method="post" action="options.php">
+            <?php
+                settings_fields('cf7ld_plugin_settings');
+                do_settings_sections('cf7ld_plugin_settings');
+                submit_button('Save Settings');
+            ?>
+        </form>
     </div>
+
 <?php } ?>
 
+<?php   
+
+// Регистрируем секции и поля настроек
+add_action('admin_init', 'cf7ld_initialize_settings');
+
+function cf7ld_initialize_settings() {
+
+    add_settings_section(
+        'cf7ld_plugin_general_settings_section',
+        'Налаштування плагіна Contact Form 7 Lead Generation',
+        'cf7ld_section_help',
+        'cf7ld_plugin_settings'
+    );
+
+     add_settings_field(
+         'ifteam_apiKey',
+         'ApiKey',
+         'np_settings_apiKey_form',
+         'cf7ld_plugin_settings',
+         'cf7ld_plugin_general_settings_section'
+    );
+
+    register_setting(
+        'cf7ld_plugin_settings',
+        'ifteam_apiKey',
+    );
+
+    function np_settings_apiKey_form() {
+        $form = get_option('ifteam_apiKey');
+    ?>
+    <input type="string" name="ifteam_apiKey" value="<?php echo esc_attr($form); ?>" />
+    <?php } ?>
+
+<?php } ?>
+
+<?php
+
+// Функция обратного вызова для секции "General Settings"
+function cf7ld_section_help() {
+    echo 'Ключ для інтеграції з ifteam ви можете отримати за посиланням ....';
+}
+
+?>
