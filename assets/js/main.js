808ca5c7c915e34559cb753c34bd72990001f2fc
pc02 <weert19933@gmail.com>
2024-04-30 17:22:14 +0300

    Интеграция с ifteam

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,55 @@
+const MAX_LENGHT_AMOUNT = 13;
+
+window.addEventListener('beforeunload', function (event) {
+	event.stopImmediatePropagation();
+});
+
 const types = [
-    'is-primary',
-    'is-link',
-    'is-info',
-    'is-success',
-    'is-warning',
-    'is-danger',
-    ]
+	'is-primary',
+	'is-link',
+	'is-info',
+	'is-success',
+	'is-warning',
+	'is-danger',
+]
 
 function displayToast(message, position, type) {
-    bulmaToast.toast({
-      message: message,
-      type: type,
-      position: position.toLowerCase().replace(' ', '-'),
-      dismissible: true,
-      duration: 4000,
-      pauseOnHover: true,
-      animate: { in: 'fadeIn', out: 'fadeOut' },
-  })
+	bulmaToast.toast({
+		message: message,
+		type: type,
+		position: position.toLowerCase().replace(' ', '-'),
+		dismissible: true,
+		duration: 4000,
+		pauseOnHover: true,
+		animate: { in: 'fadeIn', out: 'fadeOut' },
+	})
 }
+
+/**
+ * валидация для формы ввода суммы заказа
+ * */
+function validateInput(input) {
+	// Регулярное выражение для проверки чисел и шаблона [amount-product]
+	 var pattern = /^(\d{0,13}|\[amount-product\])$/;
+
+	if (!pattern.test(input)) {
+		return false;
+	}
+	return true;
+}
+
+/**
+ * Проверка ввода
+ * */
+document.getElementById('expected_budget_cf7lg').addEventListener('input', function() {
+	var input = this.value; // Получаем значение поля ввода
+	var isValid = validateInput(input); // Проверяем его на валидность
+	this.value = input.substring(0, MAX_LENGHT_AMOUNT - 1);
+	// Если ввод не прошел валидацию
+	if (!isValid && input != '') {
+		// Выводим сообщение об ошибке
+		displayToast('Пожалуйста, введите только числа или шаблон [amount-product].', 'Bottom Left', types[2]);
+		// Очищаем поле ввода
+		//this.value = '';
+	}
+});
--- includes/admin/functions.php
+++ includes/admin/functions.php
@@ -3,15 +3,46 @@
 * Получаю информацию по форме cf7lg
 * @param $id_form - это id wpcf7 контактной формы 
 */
-function get_information_the_form_cf7lg($id_form){
+function get_information_the_form_cf7lg($id_form, $json_data, $data_form){
+    $pattern = "/\[(.*?)\]/";
+    
     include_once(get_template_directory() . '/includes/database/database-cf7lg.php');
+    
     $database = new DataBaseCf7lg();
-    $result = $database->get_data_by_wpcf7_id($id_form);
+    $data = $database->get_data_by_wpcf7_id($id_form);
+    
+    $data_array = array();
+	$client = array();
+	
+    //Список форм которые заполнил клиент в плагине 
+    foreach ($data as $key => $value) {
+        $marge = $value;
+        //Если форма не пустая
+        if (!empty($value)) {
+			
+            if($key === "observers" ){
+				$data_array[$key] = explode(', ', $value);
+			}else if($key === "name" || $key === "country" || $key === "email" || $key === "phone" || $key === "client"){
+				$client[$key] = $value;
+			}else{
+				foreach ($data_form as $key_form => $replacement) {
+					$marge = str_replace("[$key_form]", $replacement, $marge);
+				}
+				
+				if(!empty($client)){
+					$data_array['client'] = $client;
+				}
+				$data_array[$key] = $marge;
+			}
+        }
+    }
 
+    $str = json_encode( $data_array, JSON_UNESCAPED_UNICODE );
     /**
      * Отправляю данные
      * */
-    mail( "serhii.kr93@gmail.com", "Test".$id_form, "id_form ".$result['params1'].' par2 '.$result['params2'].' par3 '.$result['params3'] );
+    mail( "serhii.kr93@gmail.com", "Форма заявки ", $str );
+    
 }
-
+	
 ?>
