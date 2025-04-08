b47658548963885febbc401165861be297df358e
Serhii Krainik <42071002+Sergiyss@users.noreply.github.com>
2025-04-08 09:37:07 +0300

    Update cf7-lead-generation.php

--- cf7-lead-generation.php
+++ cf7-lead-generation.php
@@ -17,8 +17,6 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit();
 }
 
-
-
 function ap_action_init(){
 // Локализация
     load_plugin_textdomain( 'cf7lg', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
@@ -34,25 +32,22 @@ function ap_action_init(){
 // Add actions
 add_action('init', 'ap_action_init');
 
-
 include_once(plugin_dir_path(__FILE__) . '/includes/admin/cf7lg-page-settings.php');
+
 if (isset($_GET['page']) && $_GET['page'] !== 'wpcf7-new'){
 	include_once(plugin_dir_path(__FILE__) . '/includes/admin/Lead-Generation-Tab_cf7lg.php');
 }
 include_once(plugin_dir_path(__FILE__) . '/includes/database/database-log-cf7lg.php'); //база данных для логов
 include_once(plugin_dir_path(__FILE__) . '/includes/database/database-cf7lg.php');
-
 //Локальные стили
 if ((isset($_GET['page']) && $_GET['page'] === 'wpcf7') || (isset($_GET['page']) &&  $_GET['page'] === 'cf7lg-plugin')) :
-
     wp_enqueue_script('bootstrap_script', '//cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.bundle.min.js', array('jquery'), false);
- 	wp_enqueue_script('bootstrap_select_script', '//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.14.0-beta2/js/bootstrap-select.min.js', array('jquery'));
+    wp_enqueue_script('bootstrap_select_script', '//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.14.0-beta2/js/bootstrap-select.min.js', array('jquery'));
     wp_enqueue_script('showdown_script', '//cdnjs.cloudflare.com/ajax/libs/showdown/1.8.6/showdown.min.js', array('jquery'), false);
     wp_enqueue_script('bulma_script', '//unpkg.com/bulma-toast', array('jquery'), false);
-
     // Регистрация и подключение стилей
     wp_enqueue_style('bootstrap_style', '//cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css');
-  	wp_enqueue_style('bootstrap_select_style', '//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.14.0-beta2/css/bootstrap-select.min.css');
+    wp_enqueue_style('bootstrap_select_style', '//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.14.0-beta2/css/bootstrap-select.min.css');
     wp_enqueue_style('bulma_style',     '//cdnjs.cloudflare.com/ajax/libs/bulma/0.9.1/css/bulma.min.css', array(), false);
     wp_enqueue_style('animate_style',   '//cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css', array(), false);
     wp_enqueue_style('main_style',      plugin_dir_url(__FILE__). 'assets/css/style.css', array(), false);
@@ -60,15 +55,12 @@ endif;
 if (isset($_GET['page']) && $_GET['page'] === 'cf7lg-plugin'):
     wp_enqueue_style('main_style',      plugin_dir_url(__FILE__). 'assets/css/style.css', array(), false);
 endif;
-
 //Глобальные стили
 wp_enqueue_script('main_script_cf7lg', plugin_dir_url( __FILE__ ) . '/assets/js/main.js', array('jquery'), '1.0', true);
 wp_enqueue_script('insert_database', plugin_dir_url( __FILE__ ) . '/assets/js/insert_database.js', array('jquery'), '1.0', true);
 wp_enqueue_script('utm_tags', plugin_dir_url( __FILE__ ) . '/assets/js/utm_tags.js', array('jquery'), '1.0', true);
 wp_enqueue_script('repeat_request', plugin_dir_url( __FILE__ ) . '/assets/js/repeat_request.js', array('jquery'), '1.0', true);
 wp_localize_script( 'js_aut', 'myAjax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' )));
-
-
 /**
  * Активация плагина
  * */
@@ -95,19 +87,11 @@ function cf7lg_plugin_menu() {
 }
 
 add_action( 'admin_menu', 'cf7lg_plugin_menu' );
-
-
-
-	
-
 //перехватчик контактной формы
 include_once(plugin_dir_path(__FILE__) . '/includes/Callback-contact-form.php');
-
 
 include_once(plugin_dir_path(__FILE__) . '/includes/admin/ajax-scripts/update_wpcf7_form.php');
 include_once(plugin_dir_path(__FILE__) . '/includes/admin/ajax-scripts/repeat_request.php');
 include_once(plugin_dir_path(__FILE__) . '/includes/admin/functions.php');
 include_once(plugin_dir_path(__FILE__) . '/includes/admin/Connects-wiht-ifteam.php');
-
-
 ?>
